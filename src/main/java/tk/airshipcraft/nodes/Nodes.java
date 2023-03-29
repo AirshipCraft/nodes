49e26bfebd89d2ae2299ce52de96f71a5b4f3bef
zeyad rashed <brwnboy2002@gmail.com>
2023-03-29 18:04:14 -0400

    enable and disable messages

--- src/main/java/tk/airshipcraft/nodes/Nodes.java
+++ src/main/java/tk/airshipcraft/nodes/Nodes.java
@@ -7,11 +7,12 @@ public final class Nodes extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("Enabled successfully!");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("Disabled!");
     }
 }
